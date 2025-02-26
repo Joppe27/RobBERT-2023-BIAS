ef5fb6b78dcff53b9925c7c7525c515a01d9fa8b
Joppe27 <joppe27@hotmail.com>
2025-02-26 23:03:59 +0100

    Add corpus extraction for verb-second word order case study

--- RobBERT-2023-BIAS.CorpusExtractor/Program.cs
+++ RobBERT-2023-BIAS.CorpusExtractor/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿#region
 
-Console.WriteLine("Hello, World!");
+using RobBERT_2023_BIAS.CorpusExtractor;
+
+#endregion
+
+if (args.Length == 0)
+    throw new ArgumentOutOfRangeException(nameof(args), "No program arguments specified");
+
+foreach (string arg in args)
+{
+    switch (arg)
+    {
+        case "--verbsecond":
+            VerbSecondExtractor.Extract();
+            break;
+        case "--subjectauxiliary":
+            break;
+        case "--genderedpronouns":
+            break;
+        default:
+            throw new ArgumentOutOfRangeException(nameof(arg), "Invalid program argument specified");
+    }
+}
+
